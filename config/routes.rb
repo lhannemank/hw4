241ea82dfa1013f4417cf0ec95e9cc67bba5f0f9
Leo Hanneman <leo.hanneman@kellogg.northwestern.edu>
2024-03-04 20:33:01 +0000

    232pm sync

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+
+  #Login and log out routes with connected actions
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+
 end
